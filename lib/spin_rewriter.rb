da1cec5b3ea62cc979fe382b41e2b1a08f8eb404
Christoph Wagner <github@pandawhisperer.net>
2020-04-29 20:18:05 -0600

    Initial commit

---
+++ lib/spin_rewriter.rb
@@ -0,0 +1,6 @@
+require "spin_rewriter/version"
+
+module SpinRewriter
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/spin_rewriter/version.rb
@@ -0,0 +1,3 @@
+module SpinRewriter
+  VERSION = "0.1.0"
+end
